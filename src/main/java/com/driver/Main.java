1c1ee54147de127cf29203d871d4845de9c51cab
shuubham <vaggysv@gmail.com>
2023-02-12 18:27:29 +0530

    second assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(7,8);
+        p.product(7,8,9);
+        p.product(7.9,8.9);
+    }
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
